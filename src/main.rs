c925c40e886a42080c9c804acf1e0029b6e7b21f
jpal91 <pallanschja@gmail.com>
2024-02-13 15:13:28 -0500

    Added initial code, types, structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#![allow(unused)]
+
+use std::collections::HashMap;
+
+use clap::Parser;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct HistoryPair(String, String);
+
+type HistoryPairs = Vec<HistoryPair>;
+type History = Vec<HistoryPairs>;
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Args {
+    /// Undo last trash command
+    #[arg(long, short)]
+    undo: bool,
+
+    /// Name of file or directory to remove
+    #[arg(required_unless_present = "undo")]
+    name: Option<String>
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
